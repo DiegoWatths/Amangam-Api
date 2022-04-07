c25cdcf9f43f5fbcdb0a643dd42fd11eaffaa272
Vic <atenciovictoralejandro@gmail.com>
2022-04-07 16:01:39 -0400

    Manga Schema

--- models/Mangas.js
+++ models/Mangas.js
@@ -1,1 +1,20 @@
-//Maybe manga schemas or chapters schemas
+const mongoose = require('mongoose');
+const {Schema, model} = require('mongoose')
+
+const MangaSchema = new Schema(
+    {
+        title: {type: String, trim: true, unique: true},
+        description: {type: String},
+        author: {type: String},
+        cover:{type: String},
+        photos: [{
+            type: String,
+            unique: true
+        }],
+        cloudinary_id: {
+            type: String,
+          }
+    }
+)
+
+module.exports = model('Manga', MangaSchema);
